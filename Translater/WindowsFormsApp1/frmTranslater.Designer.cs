b709e0cfbfa921bbfab7e9dedc338f1ad169c9f4
satwadhirs <86622261+satwadhirs@users.noreply.github.com>
2021-06-29 12:51:10 +0530

    Translate Project Added
    Translate Project Added

---
+++ Translater/WindowsFormsApp1/frmTranslater.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class frmTranslater
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.ddlSource = new System.Windows.Forms.ListBox();
+            this.ddlTarget = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(315, 197);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 0;
+            this.buttonOk.Text = "Ok";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(133, 27);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(234, 20);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(133, 73);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(234, 20);
+            this.textBoxOutput.TabIndex = 2;
+            // 
+            // ddlSource
+            // 
+            this.ddlSource.FormattingEnabled = true;
+            this.ddlSource.Location = new System.Drawing.Point(28, 114);
+            this.ddlSource.Name = "ddlSource";
+            this.ddlSource.Size = new System.Drawing.Size(120, 95);
+            this.ddlSource.TabIndex = 3;
+            // 
+            // ddlTarget
+            // 
+            this.ddlTarget.FormattingEnabled = true;
+            this.ddlTarget.Location = new System.Drawing.Point(165, 114);
+            this.ddlTarget.Name = "ddlTarget";
+            this.ddlTarget.Size = new System.Drawing.Size(120, 95);
+            this.ddlTarget.TabIndex = 4;
+            // 
+            // frmTranslater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 246);
+            this.Controls.Add(this.ddlTarget);
+            this.Controls.Add(this.ddlSource);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonOk);
+            this.Name = "frmTranslater";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmTranslater_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.ListBox ddlSource;
+        private System.Windows.Forms.ListBox ddlTarget;
+    }
+}
+
---
+++ Translater/WindowsFormsApp1/frmTranslater.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Http;
+using System.Collections;
+using System.Web.Script.Serialization;
+using System.Net;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmTranslater : Form
+    {
+        public frmTranslater()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = TranslateText(textBoxInput.Text);
+        }
+
+        public string TranslateText(string input)
+        {
+            // Set the language from/to in the url (or pass it into this function)
+            string url = String.Format
+            ("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
+             "EN", "MR", Uri.EscapeUriString(input));
+            HttpClient httpClient = new HttpClient();
+            string result = httpClient.GetStringAsync(url).Result;
+
+            // Get all json data
+            var jsonData = new JavaScriptSerializer().Deserialize<List<dynamic>>(result);
+
+            // Extract just the first array element (This is the only data we are interested in)
+            var translationItems = jsonData[0];
+
+            // Translation Data
+            string translation = "";
+
+            // Loop through the collection extracting the translated objects
+            foreach (object item in translationItems)
+            {
+                // Convert the item array to IEnumerable
+                IEnumerable translationLineObject = item as IEnumerable;
+
+                // Convert the IEnumerable translationLineObject to a IEnumerator
+                IEnumerator translationLineString = translationLineObject.GetEnumerator();
+
+                // Get first object in IEnumerator
+                translationLineString.MoveNext();
+
+                // Save its value (translated text)
+                translation += string.Format(" {0}", Convert.ToString(translationLineString.Current));
+            }
+
+            // Remove first blank character
+            if (translation.Length > 1) { translation = translation.Substring(1); };
+
+            // Return translation
+            return translation;
+        }
+
+        private void frmTranslater_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[] { new DataColumn("LanguageName", typeof(string)), new DataColumn("ISOCode", typeof(string)) });
+            dt.Rows.Add("Hindi", "HI");
+            dt.Rows.Add("English", "EN");
+            ddlSource.DataSource = dt;
+            ddlSource.ValueMember = "ISOCode";
+            ddlSource.DisplayMember = "LanguageName";
+
+            DataTable dt1 = new DataTable();
+            dt1.Columns.AddRange(new DataColumn[] { new DataColumn("LanguageName", typeof(string)), new DataColumn("ISOCode", typeof(string)) });
+            dt1.Rows.Add("Hindi", "HI");
+            dt1.Rows.Add("English", "EN");
+            dt1.Rows.Add("Marathi", "MR");
+            dt1.Rows.Add("Gujarati", "GU");
+
+            ddlTarget.DataSource = dt1;
+            ddlTarget.ValueMember = "ISOCode";
+            ddlTarget.DisplayMember = "LanguageName";
+        }
+        private void buttonOk1_Click(object sender, EventArgs e)
+        {
+            string url = "https://translation.googleapis.com/language/translate/v2?key=API Key";
+            url += "&source=" + ddlSource.SelectedValue;
+            url += "&target=" + ddlTarget.SelectedValue;
+            url += "&q=" + Uri.EscapeUriString(textBoxInput.Text);
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+            string json = client.DownloadString(url);
+            JsonData jsonData = (new JavaScriptSerializer()).Deserialize<JsonData>(json);
+            textBoxOutput.Text = jsonData.Data.Translations[0].TranslatedText;
+        }
+        public class JsonData
+        {
+            public Data Data { get; set; }
+        }
+        public class Data
+        {
+            public List<Translation> Translations { get; set; }
+        }
+        public class Translation
+        {
+            public string TranslatedText { get; set; }
+        }
+    }
+}
